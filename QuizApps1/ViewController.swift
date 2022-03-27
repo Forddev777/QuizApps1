3f36094bf14c93346f65635bf01fcfd577932606
Forddev777 <taothongdev@gmail.com>
2022-03-27 23:42:33 +0700

    update
    update

--- QuizApps1/ViewController.swift
+++ QuizApps1/ViewController.swift
@@ -6,14 +6,37 @@
 //
 
 import UIKit
-
+import AVFoundation
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Score: UILabel!
+    @IBOutlet weak var Quiztext: UILabel!
+    @IBOutlet weak var choice1: UIButton!
+    @IBOutlet weak var choice2: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateUI()
     }
-
-
+    @IBAction func choiceMade(_ sender: UIButton) {
+        let choiceAnswer = sender.currentTitle!
+        
+        
+        Timer.scheduledTimer(timeInterval: 0.2 , target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
+//        if choiceAnswer {
+//            sender.backgroundColor  = UIColor.green
+//        }else{
+//            sender.backgroundColor  = UIColor.red
+//        }
+        
+       
+    }
+    
+    @objc func updateUI(){
+        choice1.backgroundColor = UIColor.clear
+        choice2.backgroundColor = UIColor.clear
+    }
+    
+    
 }
 
